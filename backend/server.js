9edff98fd117787cbe249971c75422742e774a16
AliRaza <AliRaza192>
2025-04-26 16:15:14 +0500

    backend step

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const ConnectDB = require("./config/db");
+
+const app = express();
+// Middleware to handle CORS
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Connect to MongoDB
+ConnectDB();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
