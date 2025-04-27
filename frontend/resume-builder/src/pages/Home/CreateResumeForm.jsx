fc982cbad58f58c1a7659cc9b02617aec6805211
AliRaza <AliRaza192>
2025-04-27 23:24:33 +0500

    dashboard page UI step

--- frontend/resume-builder/src/pages/Home/CreateResumeForm.jsx
+++ frontend/resume-builder/src/pages/Home/CreateResumeForm.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/Input";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
 
 const CreateResumeForm = () => {
-  return (
-    <div>CreateResumeForm</div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [error, setError] = useState(null);
 
-export default CreateResumeForm
+  const navigate = useNavigate();
+
+  const handleCreateResume = async (e) => {
+    e.preventDefault();
+
+    if (!title) {
+      setError("Please enter resume title");
+      return;
+    }
+
+    setError("");
+
+    try {
+      const response = await axiosInstance.post(API_PATHS.RESUME.CREATE, {
+        title,
+      });
+
+      if (response.data?._id) {
+        navigate(`/resume/${response.data?._id}`);
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
+    }
+  };
+
+  return (
+    <div className="w-[90vw] md:w-[70vh] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Create New Resume</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-3">
+        Give your resume a title to get started. You can edit all details later.
+      </p>
+
+      <form onSubmit={handleCreateResume}>
+        <Input
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+          label="Resume Title"
+          placeholder="Eg: Software Engineer"
+          type="text"
+        />
+        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+        <button className="btn-primary" type="submit">
+          Create Resume
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateResumeForm;
--- frontend/resume-builder/src/pages/Home/Dashborad.jsx
+++ frontend/resume-builder/src/pages/Home/Dashborad.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import DashboradLayout from "../../components/Layouts/DashboradLayout";
+import { LuCirclePlus } from "react-icons/lu";
+import moment from "moment";
+import { useNavigate } from "react-router-dom";
+import ResumeSummaryCard from "../../components/Cards/ResumeSummaryCard";
+import CreateResumeForm from "./CreateResumeForm";
+import Model from "../../components/Model";
 
 const Dashborad = () => {
-  return (
-    <div>Dashborad</div>
-  )
-}
+  const [createOpenModel, setCreateOpenModel] = useState(false);
+  const [allResumes, setAllResumes] = useState(null);
 
-export default Dashborad
+  const navigate = useNavigate();
+
+  const fetchAllResumes = async () => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.RESUME.GET_ALL);
+      setAllResumes(response.data);
+    } catch (error) {
+      console.error("Error fetching resumes:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllResumes();
+  }, []);
+
+  return (
+    <DashboradLayout>
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4 md:gap-7 pt-1 pb-6 px-4 md:px-0">
+        <div
+          className="h-[300px] flex flex-col gap-5 items-center justify-center bg-white rounded-lg border border-purple-100 hover:border-purple-300 hover:bg-purple-50/5 cursor-pointer"
+          onClick={() => setCreateOpenModel(true)}
+        >
+          <div className="w-12 h-12 flex items-center justify-center rounded-2xl bg-purple-200/60 ">
+            <LuCirclePlus className="text-xl text-purple-500" />
+          </div>
+
+          <h3 className="font-medium text-gray-800">Add New Resume</h3>
+        </div>
+
+        {allResumes?.map((resume) => (
+          <ResumeSummaryCard
+            key={resume?._id}
+            imageUrl={resume?.thumbnailLink || null}
+            title={resume.title}
+            lastUpdated={
+              resume.updatedAt
+                ? moment(resume.updatedAt).format("DD MMM YYYY")
+                : ""
+            }
+            onSelect={() => navigate(`/resume/${resume?._id}`)}
+          />
+        ))}
+      </div>
+
+      <Model
+        isOpen={createOpenModel}
+        onClose={() => {
+          setCreateOpenModel(false);
+        }}
+        hideHeader
+      >
+        <div>
+          <CreateResumeForm />
+        </div>
+      </Model>
+    </DashboradLayout>
+  );
+};
+
+export default Dashborad;
