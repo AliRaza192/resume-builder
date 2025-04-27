7bd1d9590bdb45a1bda9a7fe7a7246177cf6d220
AliRaza <AliRaza192>
2025-04-28 00:11:34 +0500

    Resume Editor Page Structure Step

--- frontend/resume-builder/src/pages/ResumeUpdate/EditResume.jsx
+++ frontend/resume-builder/src/pages/ResumeUpdate/EditResume.jsx
@@ -1,9 +1,161 @@
-import React from 'react'
+import { useNavigate, useParams } from "react-router-dom";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  LuArrowLeft,
+  LuCircleAlert,
+  LuDownload,
+  LuPalette,
+  LuSave,
+  LuTrash2,
+} from "react-icons/lu";
+import toast from "react-hot-toast";
+import DashboradLayout from "../../components/Layouts/DashboradLayout";
+import TitleInput from "../../components/Inputs/TitleInput";
+import { useReactToPrint } from "react-to-print";
 
 const EditResume = () => {
-  return (
-    <div>EditResume</div>
-  )
-}
+  const { resumeId } = useParams();
+  const navigate = useNavigate();
 
-export default EditResume
+  const resumeRef = useRef(null);
+  const resumeDownloadRef = useRef(null);
+
+  const [baseWidth, setBaseWidth] = useState(800);
+
+  const [openThemeSelector, setOpenThemeSelector] = useState(false);
+  const [openPreview, setOpenPreview] = useState(false);
+
+  const [currentPage, setCurrentPage] = useState("profile-info");
+  const [progress, setProgress] = useState(0);
+
+  const [resumeData, setResumeData] = useState({
+    title: "",
+    thumbnailLink: "",
+    profileInfo: {
+      profileImg: null,
+      profilePreviewUrl: "",
+      fullName: "",
+      designation: "",
+      summary: "",
+    },
+    template: {
+      theme: "",
+      colorPalette: "",
+    },
+    contactInfo: {
+      email: "",
+      phone: "",
+      location: "",
+      linkedIn: "",
+      github: "",
+      website: "",
+    },
+    workExperience: [
+      {
+        companyName: "",
+        role: "",
+        startDate: "",
+        endDate: "",
+        description: "",
+      },
+    ],
+    education: [
+      {
+        degree: "",
+        institution: "",
+        startDate: "",
+        endDate: "",
+      },
+    ],
+    skills: [
+      {
+        name: "",
+        progress: 0,
+      },
+    ],
+    projects: [
+      {
+        title: "",
+        description: "",
+        github: "",
+        liveDemo: "",
+      },
+    ],
+    certifications: [
+      {
+        title: "",
+        issuer: "",
+        year: "",
+      },
+    ],
+    languages: [
+      {
+        name: "",
+        progress: 0,
+      },
+    ],
+    interests: [""],
+  });
+
+  const [errorMsg, setErrorMsg] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const validationAndNext = (e) => {};
+
+  const goToNextStep = () => {};
+
+  const goBack = (e) => {};
+
+  const renderForm = (e) => {};
+
+  const updateSection = (section, key, value) => {};
+
+  const updateArrayItem = (section, index, key, value) => {};
+
+  const addArrayItem = (section, newItem) => {};
+
+  const removeArrayItem = (section, index) => {};
+
+  const fetchResumeDetailsById = async () => {};
+
+  const uploadResumeImages = async () => {};
+
+  const uploadResumeDetails = async (thumbnailLink, profilePreviewUrl) => {};
+
+  const reactToPrintFn = useReactToPrint({ contentRef: resumeDownloadRef });
+
+  const updateBaseWidth = () => {};
+
+  useEffect(() => {
+    updateBaseWidth();
+    window.addEventListener("resize", updateBaseWidth);
+
+    if (resumeId) {
+      fetchResumeDetailsById();
+    }
+
+    return () => {
+      window.removeEventListener("resize", updateBaseWidth);
+    };
+  }, []);
+
+  return (
+    <DashboradLayout>
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between gap-5 bg-white rounded-lg border border-purple-100 py-3 px-4 mb-4">
+          <TitleInput
+            title={resumeData.title}
+            setTitle={(value) =>
+              setResumeData((prevSave) => ({
+                ...prevSave,
+                title: value,
+              }))
+            }
+          />
+        </div>
+      </div>
+    </DashboradLayout>
+  );
+};
+
+export default EditResume;
